86592a9982cee7ff37b66e78d890612b92bb0263
sumanitian <sumanprasad26042001@gmail.com>
2024-02-26 16:07:07 +0530

    updated the model

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-# model = pickle.load(open('models/model.pkl', 'rb'))
-model = pickle.load(open('models/logi.pkl', 'rb'))
+model = pickle.load(open('models/model.pkl', 'rb'))
+# model = pickle.load(open('models/logi.pkl', 'rb'))
 
 @app.route('/')
 def home():
